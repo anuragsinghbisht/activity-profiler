1e7e8e87df866f5fbc9b79f56a2b44a6cac80e07
Anurag Singh Bisht <anurag.bisht@imaginea.com>
2017-08-16 17:01:55 +0530

    Initial project setup using @angular/cli
    Add angular/material to use material design
    Add bootstrap to use grid framework
    Add @ngrx/store @ngrx/core @ngrx/effects to support redux
    Create shared and core modules

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { MaterialModule } from '@angular/material';
+
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+
+import { SharedModule } from './shared/shared.module';
+import { AppComponent } from './app.component';
+
+import 'hammerjs';
+// adding rx operators
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/finally';
+import 'rxjs/add/observable/of';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument(),
+    EffectsModule.forRoot([]),
+    MaterialModule,
+    StoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+@NgModule({
+  imports: [
+    HttpModule
+  ],
+  declarations: []
+})
+export class CoreModule { }
